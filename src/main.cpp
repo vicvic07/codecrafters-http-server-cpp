f93c640aeaebb593aea0ac5b9eff5f1b06c2c5b6
vicvic07 <victor.c.griga@gmail.com>
2026-08-15 10:02:52 +0300

    codecrafters submit [skip ci]

--- src/main.cpp
+++ src/main.cpp
@@ -20,30 +20,32 @@ std::string nextWord(int& ptr, std::string input, const char* sep)
         ret += input[ptr++];
     return ret;
 }
-std::string getBody (char *buff)
+
+std::string getBody(char* buff)
 {
-    std::string ret="";
-    int n=strlen(buff);
-    for (int i=n-1;i>=3;i--)
+    std::string ret = "";
+    int n = strlen(buff);
+    for (int i = n - 1; i >= 3; i--)
     {
         std::string is_CRLF;
-        for (int j=i-3;j<=i;j++)
+        for (int j = i - 3; j <= i; j++)
         {
-            is_CRLF+=buff[j];
+            is_CRLF += buff[j];
         }
-        if (is_CRLF=="\r\n\r\n")
+        if (is_CRLF == "\r\n\r\n")
             break;
-        ret+=buff[i];
+        ret += buff[i];
     }
-    std::reverse (ret.begin(), ret.end());
+    std::reverse(ret.begin(), ret.end());
     return ret;
 }
-std::vector<Bytef> encodeGzip (std::string input)
+
+std::vector<Bytef> encodeGzip(std::string input)
 {
     uLong source_len = input.size();
     z_stream stream{};
-    int result = deflateInit2 (&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15+16, 8, Z_DEFAULT_STRATEGY);
-    if (result==Z_OK)
+    int result = deflateInit2(&stream, Z_DEFAULT_COMPRESSION, Z_DEFLATED, 15+16, 8, Z_DEFAULT_STRATEGY);
+    if (result == Z_OK)
     {
         uLong dest_len = deflateBound(&stream, source_len);
         std::vector<Bytef> compressed(dest_len);
@@ -52,9 +54,9 @@ std::vector<Bytef> encodeGzip (std::string input)
         stream.avail_out = compressed.size();
         stream.next_out = compressed.data();
         int ret = deflate(&stream, Z_FINISH);
-        if (ret==Z_STREAM_END)
+        if (ret == Z_STREAM_END)
         {
-            compressed.resize (stream.total_out);
+            compressed.resize(stream.total_out);
             deflateEnd(&stream);
             return compressed;
         }
@@ -62,6 +64,7 @@ std::vector<Bytef> encodeGzip (std::string input)
     std::cerr << "Compression failed";
     exit(1);
 }
+
 int main(int argc, char** argv)
 {
     // Flush after every std::cout / std::cerr
@@ -100,120 +103,130 @@ int main(int argc, char** argv)
     struct sockaddr_in client_addr;
     int client_addr_len = sizeof(client_addr);
     std::cout << "Waiting for a client to connect...\n";
+    int cnt=0;
     while (true)
     {
         int client_fd = accept(server_fd, (struct sockaddr*)&client_addr, (socklen_t*)&client_addr_len);
         pid_t pid = fork();
         if (pid == 0)
         {
-            std::cout << "Client connected\n";
-            char buff[4096] = {0};
-            ssize_t nr_bytes = read(client_fd, buff, sizeof(buff));
-            if (nr_bytes == -1)
+            int crt=0;
+            std::cout << "Opened connection #" << (crt=++cnt) << "\n";
+            while (true)
             {
-                std::cerr << "Read failed\n";
-                return 1;
-            }
-            int ptr = 0;
-            std::string path = "";
-            std::string method = "";
-            method = nextWord(ptr, std::string(buff), " ");
-            std::string status = "", header = "\r\n", body = "", response = "";
-            bool encoded=0;
-            std::vector <Bytef> enc_bytes;
-            if (method == "GET")
-            {
-                for (int i = 1; i <= 1; i++)
-                    path = nextWord(ptr, std::string(buff), " ");
-                if (path.size() == 1)
-                    status = "HTTP/1.1 200 OK\r\n\r\n";
-                else
+                std::cout << "Client connected\n";
+                char buff[4096] = {0};
+                ssize_t nr_bytes = read(client_fd, buff, sizeof(buff));
+                if (nr_bytes == -1)
                 {
-                    int ptr2 = 0;
-                    std::string arg = nextWord(ptr2, path, "/");
-                    if (arg == "echo")
+                    std::cerr << "Read failed\n";
+                    return 1;
+                }
+                int ptr = 0;
+                std::string path = "";
+                std::string method = "";
+                method = nextWord(ptr, std::string(buff), " ");
+                std::string status = "", header = "\r\n", body = "", response = "";
+                bool encoded = 0;
+                std::vector<Bytef> enc_bytes;
+                if (method == "GET")
+                {
+                    for (int i = 1; i <= 1; i++)
+                        path = nextWord(ptr, std::string(buff), " ");
+                    if (path.size() == 1)
+                        status = "HTTP/1.1 200 OK\r\n\r\n";
+                    else
                     {
-                        std::string word = nextWord(ptr2, path, "/ ");
-                        status = "HTTP/1.1 200 OK\r\n";
-                        std::string req_header = "";
-                        while (ptr<nr_bytes)
+                        int ptr2 = 0;
+                        std::string arg = nextWord(ptr2, path, "/");
+                        if (arg == "echo")
                         {
-                            req_header = nextWord (ptr, std::string(buff), " :\r\n");
-                            if (req_header=="Accept-Encoding")
+                            std::string word = nextWord(ptr2, path, "/ ");
+                            status = "HTTP/1.1 200 OK\r\n";
+                            std::string req_header = "";
+                            while (ptr < nr_bytes)
                             {
-                                std::string encodings = nextWord (ptr, std::string(buff), ":\n");
-                                int ptr3=0;
-                                while (ptr3<encodings.size())
+                                req_header = nextWord(ptr, std::string(buff), " :\r\n");
+                                if (req_header == "Accept-Encoding")
                                 {
-                                    std::string encoding = nextWord (ptr3, encodings, ", \n\r");
-                                    if (encoding=="gzip")
-                                        header="Content-Encoding: gzip\r\n"+header, encoded=1;
+                                    std::string encodings = nextWord(ptr, std::string(buff), ":\n");
+                                    int ptr3 = 0;
+                                    while (ptr3 < encodings.size())
+                                    {
+                                        std::string encoding = nextWord(ptr3, encodings, ", \n\r");
+                                        if (encoding == "gzip")
+                                            header = "Content-Encoding: gzip\r\n" + header, encoded = 1;
+                                    }
                                 }
                             }
+                            if (!encoded)
+                            {
+                                body = word;
+                                header = "Content-Type: text/plain\r\nContent-Length: " + std::to_string(word.size()) +
+                                    "\r\n" +
+                                    header;
+                            }
+                            else
+                            {
+                                enc_bytes = encodeGzip(word);
+                                header = "Content-Type: text/plain\r\nContent-Length: " + std::to_string(
+                                        enc_bytes.size()) + "\r\n" +
+                                    header;
+                            }
                         }
-                        if (!encoded)
+                        else if (arg == "user-agent")
                         {
-                            body=word;
-                            header = "Content-Type: text/plain\r\nContent-Length: " + std::to_string(word.size()) + "\r\n" +
-                                header;
-                        }
-                        else
-                        {
-                            enc_bytes=encodeGzip(word);
-                            header = "Content-Type: text/plain\r\nContent-Length: " + std::to_string(enc_bytes.size()) + "\r\n" +
-                                header;
-                        }
-                    }
-                    else if (arg == "user-agent")
-                    {
-                        std::string word = "", agent = "";
-                        while (word != "User-Agent")
-                            word = nextWord(ptr, std::string(buff), " \r\n:");
-                        agent = nextWord(ptr, std::string(buff), " \r\n:");
-                        status = "HTTP/1.1 200 OK\r\n";
-                        header = "Content-Type: text/plain\r\nContent-Length: " + std::to_string(agent.size()) + "\r\n"
-                            + header;
-                        body = agent;
-                    }
-                    else if (arg == "files")
-                    {
-                        std::string file = nextWord(ptr2, path, " \r\n:/");
-                        FILE* fptr;
-                        if ((fptr = fopen((std::string(argv[2]) + file).c_str(), "r")) != NULL)
-                        {
-                            char buff[4096];
-                            while (fgets(buff, sizeof(buff), fptr))
-                                body += std::string(buff);
-                            fclose(fptr);
+                            std::string word = "", agent = "";
+                            while (word != "User-Agent")
+                                word = nextWord(ptr, std::string(buff), " \r\n:");
+                            agent = nextWord(ptr, std::string(buff), " \r\n:");
                             status = "HTTP/1.1 200 OK\r\n";
-                            header = "Content-Type: application/octet-stream\r\nContent-Length: " +
-                                std::to_string(body.size()) + "\r\n" + header;
+                            header = "Content-Type: text/plain\r\nContent-Length: " + std::to_string(agent.size()) +
+                                "\r\n"
+                                + header;
+                            body = agent;
+                        }
+                        else if (arg == "files")
+                        {
+                            std::string file = nextWord(ptr2, path, " \r\n:/");
+                            FILE* fptr;
+                            if ((fptr = fopen((std::string(argv[2]) + file).c_str(), "r")) != NULL)
+                            {
+                                char buff[4096];
+                                while (fgets(buff, sizeof(buff), fptr))
+                                    body += std::string(buff);
+                                fclose(fptr);
+                                status = "HTTP/1.1 200 OK\r\n";
+                                header = "Content-Type: application/octet-stream\r\nContent-Length: " +
+                                    std::to_string(body.size()) + "\r\n" + header;
+                            }
+                            else
+                                status = "HTTP/1.1 404 Not Found\r\n";
                         }
                         else
                             status = "HTTP/1.1 404 Not Found\r\n";
                     }
-                    else
-                        status = "HTTP/1.1 404 Not Found\r\n";
                 }
+                else if (method == "POST")
+                {
+                    std::cout << "Posting...";
+                    std::string path = nextWord(ptr, std::string(buff), " ");
+                    int ptr2 = 0;
+                    std::string file = nextWord(ptr2, path, "/");
+                    file = nextWord(ptr2, path, "/");
+                    std::fstream f((std::string(argv[2]) + file).c_str(), std::ios::out);
+                    f << getBody(buff);
+                    std::cout << getBody(buff) << "\n";
+                    status = "HTTP/1.1 201 Created\r\n";
+                }
+                response += status + header + body;
+                write(client_fd, response.c_str(), response.size());
+                if (encoded)
+                    write(client_fd, enc_bytes.data(), enc_bytes.size());
             }
-            else if (method=="POST")
-            {
-                std::cout << "Posting...";
-                std::string path = nextWord(ptr, std::string(buff), " ");
-                int ptr2=0;
-                std::string file = nextWord(ptr2, path, "/");
-                file = nextWord(ptr2, path, "/");
-                std::fstream f ((std::string(argv[2])+file).c_str(), std::ios::out);
-                f << getBody (buff);
-                std::cout << getBody (buff) << "\n";
-                status = "HTTP/1.1 201 Created\r\n";
-            }
-            response += status + header + body;
-            write(client_fd, response.c_str(), response.size());
-            if (encoded)
-                write(client_fd, enc_bytes.data(), enc_bytes.size());
+            close (client_fd);
+            std::cout << "Closed connection" << crt << "\n";
         }
-        close(client_fd);
     }
     close(server_fd);
     return 0;
